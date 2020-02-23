2f309898107c9b7b4705c9098fea7ae550f60fc5
xujiankang6 <1241450485@qq.com>
2020-02-23 19:37:07 +0800

    空对象模式study

---
+++ mystudy/src/main/java/com/jiankang/designismethod2/nullobjectpattern/Dog.java
@@ -0,0 +1,8 @@
+package com.jiankang.designismethod2.nullobjectpattern;
+
+public class Dog implements Animal {
+    @Override
+    public void noise() {
+        System.out.println("wang wang wang!");
+    }
+}
