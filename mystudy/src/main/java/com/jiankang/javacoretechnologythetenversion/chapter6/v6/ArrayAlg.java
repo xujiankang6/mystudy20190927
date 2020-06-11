7ab7cb5b3c2cb492edaf3baa73baf3ea2de88200
xujiankang6 <1241450485@qq.com>
2020-06-11 19:06:11 +0800

    merge

--- mystudy/src/main/java/com/jiankang/javacoretechnologythetenversion/chapter6/v6/ArrayAlg.java
+++ mystudy/src/main/java/com/jiankang/javacoretechnologythetenversion/chapter6/v6/ArrayAlg.java
@@ -6,4 +6,33 @@ package com.jiankang.javacoretechnologythetenversion.chapter6.v6;
  */
 
 public class ArrayAlg {
+
+    public static class Pair{
+        private double first;
+        private double second;
+
+
+        public Pair(double first, double second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        public double getFirst() {
+            return first;
+        }
+
+        public double getSecond() {
+            return second;
+        }
+    }
+
+    public static Pair minmax(double[] values){
+        double min = Double.POSITIVE_INFINITY;
+        double max = Double.NEGATIVE_INFINITY;
+        for (double v : values) {
+            if(min>v) min = v;
+            if(max<v) max = v;
+        }
+        return new Pair(min,max);
+    }
 }
--- mystudy/src/main/java/com/jiankang/javacoretechnologythetenversion/chapter6/v6/StaticInnerClassTest.java
+++ mystudy/src/main/java/com/jiankang/javacoretechnologythetenversion/chapter6/v6/StaticInnerClassTest.java
@@ -6,4 +6,16 @@ package com.jiankang.javacoretechnologythetenversion.chapter6.v6;
  */
 
 public class StaticInnerClassTest {
+    public static void main(String[] args) {
+        double[] d =new double[20];
+        for(int i=0;i<d.length;i++){
+            d[i] = 100* Math.random();
+        }
+        ArrayAlg.Pair p = ArrayAlg.minmax(d);
+        System.out.println("min: "+p.getFirst());
+        System.out.println("max: "+p.getSecond());
+
+        ArrayAlg.Pair p1 =new ArrayAlg.Pair(1,2);
+        System.out.println(p1.getSecond());
+    }
 }
