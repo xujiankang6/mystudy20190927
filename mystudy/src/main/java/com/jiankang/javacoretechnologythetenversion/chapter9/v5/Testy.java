463cd41c6b4a445b782a7590dfd79ee0a6f21f8d
xujiankang6 <1241450485@qq.com>
2020-07-02 19:19:49 +0800

    阻塞队列学习

--- mystudy/src/main/java/com/jiankang/javacoretechnologythetenversion/chapter9/v5/Testy.java
+++ mystudy/src/main/java/com/jiankang/javacoretechnologythetenversion/chapter9/v5/Testy.java
@@ -18,10 +18,17 @@ public class Testy {
         List<String> b = new ArrayList<String>();
         b.add("aa");
         b.add("bb");
-        b.add("ccc");
+        b.add("cc");
         //求交集,业务中经常会用到
+//        第一步：将其中一个集合放入Set里面
+//        第二步：执行retainAll()方法
+//        第三步:查看该set集合的个数是否大于1
         Set<String> result = new HashSet<>(a);
+        System.out.println(result);
         result.retainAll(b);
+        if (result.size()>0){
+            System.out.println("集合a和集合b有交集" );
+        }
         System.out.println(result);
         String[] array = result.toArray(new String[0]);
         HashSet<String> strings = new HashSet<>(Arrays.asList(array));
