3f690f411014ed091db544bfa559170294234cda
xujiankang6 <1241450485@qq.com>
2020-08-05 17:53:28 +0800

    spring 起步 第一章

---
+++ mystudy/src/main/java/com/jiankang/springpracticesversion5/chapter1/TacoCloud/src/test/java/tacos/HomeControllerTest.java
@@ -0,0 +1,36 @@
+package tacos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+/*
+ *@create by jiankang
+ *@date 2020/8/5 time 17:05
+ */
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
+public  class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception {
+
+        mockMvc.perform(get("/")).andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(
+                        containsString("Welcome to ...")));
+    }
+
+
+}
---
+++ mystudy/src/main/java/com/jiankang/springpracticesversion5/chapter1/TacoCloud/src/test/java/tacos/TacoCloudApplicationTests.java
@@ -0,0 +1,14 @@
+package tacos;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
